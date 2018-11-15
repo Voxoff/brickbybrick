d1775e046996fcfb9c6964fdbdbce54443843231
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-11-15 17:35:30 +0000

    rails is the best ever; marju must learn this;

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  post "/login", to: 'users#login'
-  get "/current_user", to: 'users#get_current_user'
-  resources :users
+  resources :users do
+    collection do
+      post 'login'
+      get 'target'
+      get 'current_user'
+    end
+  end
   resources :targets, except: [:index]
-  get "/current_user_target", to: 'users#target'
+  # get "/current_user", to: 'users#get_current_user'
+  # post "/login", to: 'users#login'
+  # get "/current_user_target", to: 'users#target'
 end
